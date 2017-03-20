b3900cd16e70642cbb48ff6bb41a2af60ded2fea
tylermooney86 <tyler.mooney@colorado.edu>
2017-03-20 12:49:07 -0600

    fixed html style bug

--- WebApp/Server.js
+++ WebApp/Server.js
@@ -3,6 +3,8 @@ var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 
+app.use(express.static( __dirname + '/styles'));
+
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
