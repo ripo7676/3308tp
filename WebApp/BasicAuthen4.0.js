a51318e8fb7214239352c3da823ce99ce1d74b47
Patrick Baier <baier145@gmail.com>
2017-04-05 12:36:13 -0600

    Adding session data to BasicAuthen4.0.js

--- WebApp/BasicAuthen4.0.js
+++ WebApp/BasicAuthen4.0.js
@@ -8,6 +8,14 @@ app.use(function(req, res, next) {
         res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
         res.end('Unauthorized');
     } else {
+	var NodeSession = require('node-session'); // Found on https://github.com/quorrajs/NodeSession
+	var result=' '; //Result will store a random 32 character string
+	var chars='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' //Needed to generate random string
+	for (var i=0;i<32;i++) result = result + chars[Math.floor(Math.random()*66) // Random string generation. 66 is the length of chars
+	session = new NodeSession({secret: result});
+	session.startSession(req, res, callback)
+	req.session.put('username');
+	req.session.push(user); //Should push the username onto the session.username variable 
         next(); //continue instance
     }
 });
