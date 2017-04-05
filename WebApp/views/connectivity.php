2f015b837d3f353e83ac7ca3f12d49ae6617d6cc
tylermooney86 <tyler.mooney@colorado.edu>
2017-04-05 14:12:33 -0600

    edited connectivity.php

--- WebApp/views/connectivity.php
+++ WebApp/views/connectivity.php
@@ -2,8 +2,8 @@
 
 define('DB_HOST', 'localhost'); 
 define('DB_NAME', 'Login'); 
-define('DB_USER','root'); 
-define('DB_PASSWORD','Mistycat1'); 
+define('DB_USER','User'); 
+define('DB_PASSWORD','Password'); 
 
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
