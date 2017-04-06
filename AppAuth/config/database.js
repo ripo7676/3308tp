98a5c52eaf77c24e203a5999e240f00ce52b6c04
Josh Trujillo <Jotr4184@colorado.edu>
2017-04-06 16:24:30 -0600

    Added connection to MongoDB server hosting on mLab
    Automatically POSTs and is able to query our database for to authenticate users. No local MongoDB needed!!

--- AppAuth/config/database.js
+++ AppAuth/config/database.js
@@ -1,8 +1,9 @@
 // config/database.js
 //module.exports = 'mongodb://localhost/db_name'
 module.exports = {
-	url : 'mongodb://127.0.0.1:27017/test'
+	url : 'mongodb://Jotr4184:CSCI3308TEAM1107@ds153730.mlab.com:53730/team1107test'
 };
 
+// 'mongodb://127.0.0.1:27017/test'
 // 'mongodb://<Jotr4184>:<CSCI3308TEAM1107>@ds153730.mlab.com:53730/team1107test' 
 // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
