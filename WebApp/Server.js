136cd1d902427f4f2384a0b3c52d2c6416582fbb
Richard Poulson <Rick.Poulson@colorado.edu>
2017-04-09 13:29:19 -0600

    changed how user[name] and user[pass] are handled after posting

--- WebApp/Server.js
+++ WebApp/Server.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
+var bodyParser = require('body-parser')
 var path = __dirname + '/views/';
 
 app.use(express.static( __dirname + '/styles'));
+app.use(bodyParser.json());           // For parsing JSON post data
+app.use(bodyParser.urlencoded({extended: true})); // Also needed for post data
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -26,7 +29,9 @@ router.get("/login",function(req,res){
   res.sendFile(path + "login.html");
 });
 
-router.post("/login",function(req,res){
+app.post("/login",function(req,res){
+  console.log(req.body.user.name)
+  console.log(req.body.user.pass)
   res.sendFile(path + "login.html");
 });
 
