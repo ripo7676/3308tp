fa403f0521ee4d4e97eedd28c334b13c6a0a960b
Richard Poulson <Rick.Poulson@colorado.edu>
2017-04-09 18:27:08 -0600

    just finished implementing authentication

--- WebApp/Server.js
+++ WebApp/Server.js
@@ -29,10 +29,26 @@ router.get("/login",function(req,res){
   res.sendFile(path + "login.html");
 });
 
-app.post("/login",function(req,res){
-  console.log(req.body.user.name)
-  console.log(req.body.user.pass)
-  res.sendFile(path + "login.html");
+app.post("/login",function(req, res, next) {
+  var user = req.body.user;
+  if (user === undefined || user['name'] !== 'username' || user['pass'] !== 'password') {
+          res.statusCode = 401;
+          res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+          res.end('Unauthorized');
+  }
+  else {
+  	var NodeSession = require('node-session'); // Found on https://github.com/quorrajs/NodeSession
+  	var result=''; //Result will store a random 32 character string
+  	var chars='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' //Needed to generate random string
+  	for (var i=0;i<32;i++) result = result + chars[Math.floor(Math.random()*66)] // Random string generation. 66 is the length of chars
+  	session = new NodeSession({secret: result});
+  	session.startSession(req, res, next)
+    session.username = user['name'];
+  	//req.session.push(user); //Should push the username onto the session.username variable
+    console.log("A session has been created!\n")
+    console.log(session)
+    res.redirect('/');
+  }
 });
 
 app.use("/",router);
