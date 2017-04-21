dbcc37d98c16f8cf3baffbadeb52584464a1a01c
Richard Poulson <rick.poulson@colorado.edu>
2017-04-21 12:43:03 -0600

    trying new DB schema

--- AppAuth/app/models/user.js
+++ AppAuth/app/models/user.js
@@ -6,28 +6,35 @@ var bcrypt   = require('bcrypt-nodejs');
 // define the schema for our user model
 var userSchema = mongoose.Schema({
 
-    local            : {
-        email        : String,
-        password     : String,
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
+  local              : {
+    email            : String,
+    password         : String
+  },
+  facebook           : {
+    id               : String,
+    token            : String,
+    email            : String,
+    name             : String
+  },
+  twitter            : {
+    id               : String,
+    token            : String,
+    displayName      : String,
+    username         : String,
+    tweets           : {
+      categories     : [String],
+      messages       : [{
+        category     : String,
+        body         : String
+      }]
     }
+  },
+  google             : {
+    id               : String,
+    token            : String,
+    email            : String,
+    name             : String
+  }
 
 });
 
