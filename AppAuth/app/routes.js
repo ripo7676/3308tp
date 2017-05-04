9ac82294fd7061a6248364412384d9f788907c4b
Jotr4184 <Jotr4184@colorado.edu>
2017-05-04 14:46:05 -0600

    add twitter routes

--- AppAuth/app/routes.js
+++ AppAuth/app/routes.js
@@ -21,7 +21,7 @@ module.exports = function(app, passport) {
 
     // process the login form
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/profile', // redirect to the secure profile
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
@@ -32,7 +32,6 @@ module.exports = function(app, passport) {
     // show the signup form
     app.get('/signup', function(req, res) {
 
-        // render the page and pass in any flash data if it exists
         res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
 
@@ -46,8 +45,8 @@ module.exports = function(app, passport) {
     // =====================================
     // PROFILE SECTION =====================
     // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
+    // protected so you have to be logged in to visit
+    // use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
             user : req.user // get the user out of session and pass to template
@@ -64,12 +63,24 @@ module.exports = function(app, passport) {
             user : req.user // get the user out of session and pass to template
         });
     });
+	
+	// =====================================
+    // TWITTER ROUTES ======================
+    // =====================================
+    // route for twitter authentication and login
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+
+    // handle the callback after twitter has authenticated the user
+    app.get('/auth/twitter/callback',
+        passport.authenticate('twitter', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
 
 	// =====================================
     // POST TO TWITTER SECTION =============
     // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/post', isLoggedIn, function(req, res) {
 		  // dictionary of categories/messages, seems easer to parse than 'user'
 		var thisUserJSON = JSON.stringify(req.user.twitter.tweets.categories)
