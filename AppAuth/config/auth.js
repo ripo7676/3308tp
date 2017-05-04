92a7905848bbd0826ce04e6d87b6a719f3c4fc5b
Jotr4184 <Jotr4184@colorado.edu>
2017-05-04 14:17:32 -0600

    getting twitter auth working

---
+++ AppAuth/config/auth.js
@@ -0,0 +1,10 @@
+// deliver config to our application directly via API keys
+module.exports = {
+	
+    'twitterAuth' : {
+        'consumerKey'      : 'gSsS6rSmJQHSBBriu3U5qYIoH',
+        'consumerSecret'   : 'HpsgEaDHpco4Y1YfhkS2v1GpAdZbhw3w56MO6QtdxnhTP0LVVE',
+        'callbackURL'      : 'http://127.0.0.1:8080/auth/twitter/callback'
+    }
+
+};
